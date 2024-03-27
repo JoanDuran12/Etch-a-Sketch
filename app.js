23d6643026eb57e05d136b757118b4feb2f09397
Joan Duran <joanmanuel808@gmail.com>
2024-03-27 14:30:08 -0400

    Fixed grid creation using flex

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
 
+const sketch = document.querySelector(".sketch-container");
+
+
+
 function createSketch(size) {
-
-    const sketch = document.querySelector(".sketch-container");
-    let divNum = 0;
-
     for(let i = 0; i < (size * size); i++) {
         const div = document.createElement("div");
         div.classList.add("sketchItem");
-        // div.style.padding = "30px"
-        // div.style.height = "30px";
-        // div.style.width = "30px";
-        div.style.flexBasis = "auto";
-        divNum++;
-        div.textContent = divNum;
+        div.style.width = (sketch.clientWidth / size) + "px";
         sketch.appendChild(div);
     }
 }
 
-createSketch(16);
+createSketch(8);
