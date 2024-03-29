e6281b68a29fe4c577c2119dd6d149b1f91702ad
Joan Duran <joanmanuel808@gmail.com>
2024-03-28 21:03:58 -0400

    Added initial hover effect to the grid

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 
 const sketch = document.querySelector(".sketch-container");
+const colorPad = document.querySelector(".colorPad");
 
-
-
+// Create a new grid
 function createSketch(size) {
     for(let i = 0; i < (size * size); i++) {
         const div = document.createElement("div");
         div.classList.add("sketchItem");
-        div.style.width = (sketch.clientWidth / size) + "px";
+        div.style.flexBasis = (sketch.clientWidth / size) + "px";
+        // hoverEffectOn(div);
         sketch.appendChild(div);
     }
 }
 
-createSketch(8);
+// Apply selected color to the corresponding cell
+function hoverEffectOn(item){
+    const color = colorPad.value;
+    item.addEventListener("mouseover", () => {
+        item.style.backgroundColor = color;
+    })
+}
+
+// function hoverEffectOff(item){
+//     const color = colorPad.value;
+//     item.addEventListener("mouseover", () => {
+//         this.style.color = "black";
+//     })
+// }
+
+// Create a starting grid of 16x16
+createSketch(32);
+
+colorPad.addEventListener("click", () =>{
+    const color = colorPad.value;
+    hoverEffectOn(div, color);
+})
