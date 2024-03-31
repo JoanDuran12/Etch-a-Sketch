9ef7d85bc87b9a60f1f2124ec79595fe2c9f2014
Joan Duran <joanmanuel808@gmail.com>
2024-03-30 23:21:23 -0400

    Implemented eraser and clear grid functions

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 
-// Elements used
+// Dom elements used
 const sketch = document.querySelector(".sketch-container");
 const colorPad = document.querySelector(".colorPad");
-const sizeBar = document.querySelector(".gridSize-bar")
-const sizeTitle = document.querySelector(".size-title")
+const sizeBar = document.querySelector(".gridSize-bar");
+const sizeTitle = document.querySelector(".size-title");
+const randomColor = document.querySelector(".btn-random");
+const darker = document.querySelector(".btn-darker");
+const eraser = document.querySelector(".btn-eraser");
+const clear = document.querySelector(".btn-clear");
+
 let color = colorPad.value;
+
+let boolRandom = false;
+let boolDarker = false;
+let boolEraser = false;
 
 // -----------------------------------FUNCTIONS-------------------------------------------------
 
 // Create a new grid
 function createGrid(size) {
+    deleteActualGrid()
     for(let i = 0; i < (size * size); i++) {
         const div = document.createElement("div");
         div.classList.add("sketchItem");
@@ -27,8 +37,24 @@ function hoverEffectOn(item){
 };
 
 // Update future grid background color with selected color
-function updateColor(){
-    color = colorPad.value;
+function updateColor(e){
+    if(boolRandom){
+        let randomR = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        color =  `rgb(${randomG}, ${randomB}, ${randomR})`;
+        boolRandom = false;
+    }
+    else if(boolDarker){
+        boolDarker = false;
+    }
+    else if(boolEraser){
+        color = "#FFFFFF"
+        boolEraser = false;
+    }
+    else{
+        color = colorPad.value;
+    }
 };
 
 // Delete actual grid
@@ -44,11 +70,27 @@ createGrid(16);
 // Update the current color
 colorPad.addEventListener("input", updateColor);
 
+// randomColor.addEventListener("click", () =>{
+//     boolRandom = true;
+//     updateColor();
+// });
+
+// darker.addEventListener("click", () =>{
+//     boolDarker = true;
+//     updateColor();
+// });
+
+eraser.addEventListener("click", () =>{
+    boolEraser = true;
+    updateColor();
+});
+
+// Handle Clear btn -> Clear the entire sketch pad
+clear.addEventListener("click", () => {createGrid(sizeBar.value)});
+
 // Create a new grid according to what the user range is
 sizeBar.addEventListener("mousemove", () => {
     sizeTitle.textContent = `Grid Size: ${sizeBar.value}x${sizeBar.value}`
 });
-sizeBar.addEventListener("change", () => {
-    deleteActualGrid();
-    createGrid(sizeBar.value);
-})
+
+sizeBar.addEventListener("change", () => {createGrid(sizeBar.value)});
